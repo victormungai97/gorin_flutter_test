97699e7d1d83697ee2089eaca1e7e995662cb767
Victor Kamau <victormungai97@gmail.com>
2023-10-14 12:34:29 +0300

    feat: :sparkles: Setup Firebase
    Added and configured core Firebase functionality for Flutter on Android, iOS and Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gorin_test_project/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
+/// Root widget for the application
 class App extends StatelessWidget {
+  /// Constructor for ``App``
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
+      providers: const [
         /*BlocProvider(
           create: (context) => SubjectBloc(),
         ),
